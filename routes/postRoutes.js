21d2f7e57d41f62ea05da7be2ef9ae22adcc0ce5
Cyb3r-Ali <fireflyali19@gmail.com>
2024-03-27 10:54:09 +0500

    Added routes for posts

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,7 +4,13 @@ const authenticateToken = require('../middlewares/verifyToken');
 const postController = require('../controllers/postController');
 
 // Route to create a new post
-router.post('/posts', authenticateToken, postController.createPost);
+router.post('/post', authenticateToken, postController.createPost);
+
+// Route to get all posts
+router.get('/posts', authenticateToken, postController.getPosts);
+
+// Route to get single post 
+router.get('/posts/:id', authenticateToken, postController.getPost);
 
 // Other routes for updating, deleting, and retrieving posts can be added here
 
