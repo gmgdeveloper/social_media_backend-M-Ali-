43fbeb755d5c1ffac5450b2daa236fd995d49599
Cyb3r-Ali <fireflyali19@gmail.com>
2024-04-15 14:06:55 +0500

    added an admin check middleware

--- middlewares/adminCheck.js
+++ middlewares/adminCheck.js
@@ -1,22 +1,23 @@
 const adminCheck = (req, res, next) => {
     const isAdmin = req.user.is_admin;
-    const is_Admin = req.user.role
+    const role = req.user.role;
 
-    if (isAdmin === undefined && is_Admin === undefined || isAdmin === null && is_Admin === null) {
+    if (isAdmin === undefined && role === undefined || isAdmin === null && role === null) {
         return res.status(401).json({ status: 401, error: 'Access denied. Please log in first.' });
     }
 
-    if (isAdmin !== 1) {
+    if (isAdmin !== 1 && role !== "admin") {
         return res.status(403).json({ status: 403, error: 'Access denied. Only admins are allowed to perform this action.' });
     }
 
-    if (isAdmin === 1 && is_Admin == "admin") {
+    if (isAdmin === 1 && role === "admin") {
         next();
     } else {
         err = {
             status: 403,
             error: 'Access denied. Only administrators are allowed to perform this action.'
         }
+        return res.status(403).json(err);
     }
 };
 
